1e69cc6c4f549fbb1e27083cbf1d61d2d68be9ac
Sokrady Chey <sokradychey@gmail.com>
2021-05-15 15:03:54 -0400

    JS for adding new entry

---
+++ index.js
@@ -0,0 +1,13 @@
+// Creating a new entry 
+function newEntry() {
+    let li = document.createElement("li");
+    let entryValue = document.getElementById("bglevel").value;
+    let text = document.createTextNode(entryValue);
+    li.appendChild(text);
+    if (entryValue === '') {
+        alert("Add your BG Levels");
+    } else {
+        document.getElementById("entryList").appendChild(li);
+    }
+    document.getElementById("entryValue").value = "";
+}

f6ea5273ce02085fc3b0ca96e9b6f26935a1795e
Sokrady Chey <sokradychey@gmail.com>
2021-05-18 11:05:34 -0400

    creating an array from the blood sugar levels

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 // Creating a new entry 
-// function newEntry() {
-//     document.getElementById('displayLevels').innerHTML = 
-//     document.getElementById('levels').value;
-
-//     document.getElementById('displayTime').innerHTML = 
-//     document.getElementById('time').value;
-
-//     document.getElementById('displayMeal').innerHTML = 
-//     document.getElementById('meal').value;
-
-//     document.getElementById('displayNotes').innerHTML = 
-//     document.getElementById('notes').value;
-// }
-
 let trackingValue = document.querySelector('#trackingValue');
 let displayLevels = document.querySelector('#levels');
-let displayTime = document.querySelector('#time');
-let displayMeal = document.querySelector('#meal');
+// let displayTime = document.querySelector('#time');
+// let displayMeal = document.querySelector('#meal');
 let trackingList = document.querySelector('#trackingList');
 
 trackingValue.addEventListener('submit', function(e) {
@@ -25,19 +11,19 @@ trackingValue.addEventListener('submit', function(e) {
 
     // Ignore if form is empty
     if (displayLevels.value.length < 1) return;
-    if (displayTime.value.length < 1) return;
-    if (displayMeal.value.length < 1) return;
+    // if (displayTime.value.length < 1) return;
+    // if (displayMeal.value.length < 1) return;
 
     // Add Blood Sugar Values
     trackingList.innerHTML += 
-    '<li>' + displayLevels.value + '</li>' + 
-    '<li>' + displayTime.value + '</li>' + 
-    '<li>' + displayMeal.value + '</li>';
+    '<li>' + displayLevels.value + '</li>';
+    // '<li>' + displayTime.value + '</li>' + 
+    // '<li>' + displayMeal.value + '</li>';
 
     // Clear Inputs
     displayLevels.value = '';
-    displayTime.value = '';
-    displayMeal.value = '';
+    // displayTime.value = '';
+    // displayMeal.value = '';
 
     // Save Values to Local Storage
     localStorage.setItem('displayLevels', trackingList.innerHTML);
@@ -48,13 +34,21 @@ trackingValue.addEventListener('submit', function(e) {
 const savedValues = localStorage.getItem('displayLevels');
 
 // If there are any saved items, update our list
-if (savedValues) {
-	trackingList.innerHTML = savedValues;
-}
+// if (savedValues) {
+// 	trackingList.innerHTML = savedValues;
+// }
 
 // Clear Form
 function clearForm() {
     document.getElementById('levels').value = '';
-    document.getElementById('time').value = '';
-    document.getElementById('meal').value = '';
+    // document.getElementById('time').value = '';
+    // document.getElementById('meal').value = '';
+}
+
+// Creating an array from form data
+var levels = [];
+function submitLevels() {
+    sugarLevels = document.getElementById('levels').value;
+    levels.push(sugarLevels);
+    console.log(levels);
 }

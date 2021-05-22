da7a9d335721f74390d709463851e89781bd76d8
Sokrady Chey <sokradychey@gmail.com>
2021-05-21 20:48:49 -0400

    Added Conditional Statements based on type of meal for each input

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Creating a new entry 
 let trackingValue = document.querySelector('#trackingValue');
 let displayLevels = document.querySelector('#levels');
+let displayMeals = document.querySelector('#meal');
 let trackingList = document.querySelector('#trackingList');
 
 trackingValue.addEventListener('submit', function(e) {
@@ -11,11 +12,12 @@ trackingValue.addEventListener('submit', function(e) {
     if (displayLevels.value.length < 1) return;
 
     // Add Blood Sugar Values
-    trackingList.innerHTML += displayLevels.value + ' ' + 'mg/dL' + '<br>';
+    trackingList.innerHTML += (`${displayLevels.value} mg/dL` + '<br>');
 
     // Clear Inputs
     displayLevels.value = '';
-
+    displayMeals.value = '';
+    
     // Save Values to Local Storage
     localStorage.setItem('displayLevels', trackingList.innerHTML);
 
@@ -48,8 +50,19 @@ function submitLevels() {
     }
 
     let avg = total / levels.length;
+    document.getElementById('dailyAvg').innerHTML = (`${avg} mg/dL`);
+    
 
-    document.getElementById('dailyAvg').innerHTML = avg + ' ' + 'mg/dL';
+    if (document.getElementById('meal').value == document.getElementById('fastingValue').value) {
+        document.getElementById('dailyFasting').innerHTML = (`${sugarLevels} mg/dL`);
+    } else if (document.getElementById('meal').value == document.getElementById('postBreakfast').value) {
+        document.getElementById('dailyBreakfast').innerHTML = (`${sugarLevels} mg/dL`);
+    } else if (document.getElementById('meal').value == document.getElementById('postLunch').value) {
+        document.getElementById('dailyLunch').innerHTML = (`${sugarLevels} mg/dL`);
+    } else if (document.getElementById('meal').value == document.getElementById('postDinner').value) {
+        document.getElementById('dailyDinner').innerHTML = (`${sugarLevels} mg/dL`);
+    } else { document.getElementById('dailyAvg').innerHTML = (`${avg} mg/dL`); };
+
 
     console.log(`the total is ${total}`);
     console.log(`the average is ${avg}`);
